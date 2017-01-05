b2611bc183df1300d448211b5eb22bdeb984e568
weeged <weeged@gmail.com>
2017-01-05 12:21:27 +0000

    Signed-off-by: weeged <weeged@gmail.com>

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func defaultHandler(w http.ResponseWriter, r *http.Request) {
 <<<<<<< HEAD
     fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!  Shmersh!!!</h1>\n")
 =======
-    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!  Shmersh!!</h1>\n")
+    fmt.Fprintf(w, "<h1>Hello from Cisco Shipped!  Shmersh!! CG</h1>\n")
 >>>>>>> 16b5679ae7334e9c4dde4acc2301180331f8f5fe
 }
 
